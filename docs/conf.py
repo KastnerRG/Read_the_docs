8c5812850195e53bf7ff2b36106cc3f592523321
codspalaniappan95 <spalania@ucsd.edu>
2020-01-09 10:35:54 -0800

    Modified conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -141,8 +141,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'fir.tex', 'fir Documentation',
-     'Alireza Khodamoradi', 'manual'),
+    (master_doc, 'cse237c.tex', 'CSE237c Documentation',
+     'Sivasankar Palaniappan', 'manual'),
 ]
 
 
@@ -151,7 +151,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'fir', 'fir Documentation',
+    (master_doc, 'cse237c', 'CSE237c Documentation',
      [author], 1)
 ]
 
@@ -162,8 +162,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'fir', 'fir Documentation',
-     author, 'fir', 'One line description of project.',
+    (master_doc, 'cse237c', 'CSE237c Documentation',
+     author, 'cse237c', 'One line description of project.',
      'Miscellaneous'),
 ]
 
