1909843a3d01209380fa4c5e2c70399b313925d5
ZhenghuaMa <54973815+zhenghuama@users.noreply.github.com>
2023-10-29 17:02:58 -0700

    Update script.tcl
    fix device

--- project_files/project3/dft_1024_precomputed/script.tcl
+++ project_files/project3/dft_1024_precomputed/script.tcl
@@ -11,7 +11,7 @@ add_files coefficients1024.h
 add_files -tb out.gold.dat
 add_files -tb dft_test.cpp
 open_solution "solution1"
-set_part {xc7z020clg484-1}
+set_part {xc7z020clg400-1}
 create_clock -period 10 -name default
 source "./directives.tcl"
 
