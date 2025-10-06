a7c8e9a30f9cf6e479735c39a24e5cc8e138f53d
Mayank Kumar <mayank.kumar1123@gmail.com>
2025-10-06 16:32:43 -0700

    Convert indents to spaces

--- project_files/project1/fir11/fir.cpp
+++ project_files/project1/fir11/fir.cpp
@@ -1,12 +1,12 @@
 /*
-	Filename: fir.cpp
-		FIR lab wirtten for WES/CSE237C class at UCSD.
+    Filename: fir.cpp
+        FIR lab wirtten for WES/CSE237C class at UCSD.
 
-	INPUT:
-		x: signal (chirp)
+    INPUT:
+        x: signal (chirp)
 
-	OUTPUT:
-		y: filtered output
+    OUTPUT:
+        y: filtered output
 
 */
 
@@ -17,25 +17,25 @@ void fir (
   data_t x
   )
 {
-	coef_t c[N] = {53, 0, -91, 0, 313, 500, 313, 0, -91, 0,53};
-	// Write your code here
-	static
-		data_t shift_reg[N];
-		acc_t acc;
-		int i;
+    coef_t c[N] = {53, 0, -91, 0, 313, 500, 313, 0, -91, 0,53};
 
-	acc = 0;
-	Shift_Accum_Loop:
-	for (i = N - 1; i >= 0; i--){
-		if (i == 0) {
-			acc += x * c[0];
-			shift_reg[0] = x;
-		} else {
-			shift_reg[i] = shift_reg[i - 1];
-			acc += shift_reg[i] * c[i];
-		}
-	}
-	*y = acc;
+    // Write your code here
+    static data_t shift_reg[N];
+    acc_t acc;
+    int i;
+
+    acc = 0;
+    Shift_Accum_Loop:
+    for (i = N - 1; i >= 0; i--){
+        if (i == 0) {
+            acc += x * c[0];
+            shift_reg[0] = x;
+        } else {
+            shift_reg[i] = shift_reg[i - 1];
+            acc += shift_reg[i] * c[i];
+        }
+    }
+    *y = acc;
 }
 
 
