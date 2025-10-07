5f134fdb9b670edbc9866582fdc0cc871c05c232
Shail <shvaidya@ucsd.edu>
2025-10-06 22:31:50 -0700

    Project1_Question4

---
+++ project_files/project1/fir128_q4/fir.cpp
@@ -0,0 +1,38 @@
+/*
+    Filename: fir.cpp
+        FIR lab wirtten for WES/CSE237C class at UCSD.
+        Match filter
+    INPUT:
+        x: signal (chirp)
+
+    OUTPUT:
+        y: filtered output
+
+*/
+
+#include "fir.h"
+
+void fir (
+  data_t *y,
+  data_t x
+  )
+{
+
+    coef_t c[N] = {10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+    
+    // Write your code here
+    static data_t shift_reg[N];
+    acc_t acc;
+    int i;
+
+    acc = 0;
+    Shift_Accum_Loop:
+    for (i = N - 1; i > 0; i--) {
+        shift_reg[i] = shift_reg[i - 1];
+        acc += shift_reg[i] * c[i];
+    }
+    acc += x * c[0];
+    shift_reg[0] = x;
+    *y = acc;
+}
+
