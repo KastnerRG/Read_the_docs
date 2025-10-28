8b082d2e50af0b611983eb44c4f273d6c37817fd
zhenghuama <mazh17da@gmail.com>
2025-10-28 00:16:21 -0700

    Updated DFT1024 document

--- project_files/project3/dft_1024_precomputed/dft.h
+++ project_files/project3/dft_1024_precomputed/dft.h
@@ -2,5 +2,6 @@
 
 typedef float DTYPE;
 #define SIZE 1024 		/* SIZE OF DFT */
+
 void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE], DTYPE imag_op[SIZE]);
 // Use streaming interface for demo
